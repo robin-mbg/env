d03ee93eb2d622c89f6840ab257adcbc73ca1323
Robin Meiseberg <rome@netlight.com>
2022-08-03 14:58:21 +0200

    basic env parsing functions

---
+++ env.go
@@ -0,0 +1,131 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func GetStringOrError(key string) (string, error) {
+	value := os.Getenv(key)
+	if value == "" {
+		return "", fmt.Errorf("environment variable %v is empty", key)
+	}
+
+	return value, nil
+}
+
+func GetStringOrDefault(key string, defaultValue string) string {
+	value, err := GetStringOrError(key)
+	if err != nil {
+		return defaultValue
+	}
+
+	return value
+}
+
+func GetStringOrPanic(key string) string {
+	value, err := GetStringOrError(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return value
+}
+
+func GetBoolOrError(key string) (bool, error) {
+	value := os.Getenv(key)
+	if value == "" {
+		return false, fmt.Errorf("environment variable %v is empty", key)
+	}
+
+	boolValue, err := strconv.ParseBool(value)
+	if err != nil {
+		return false, fmt.Errorf("environment variable %v could not be parsed to boolean value", key)
+	}
+
+	return boolValue, nil
+}
+
+func GetBoolOrDefault(key string, defaultValue bool) bool {
+	value, err := GetBoolOrError(key)
+	if err != nil {
+		return defaultValue
+	}
+
+	return value
+}
+
+func GetBoolOrPanic(key string) bool {
+	value, err := GetBoolOrError(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return value
+}
+
+func GetIntOrError(key string) (int, error) {
+	value := os.Getenv(key)
+	if value == "" {
+		return 0, fmt.Errorf("environment variable %v is empty", key)
+	}
+
+	intValue, err := strconv.ParseInt(value, 10, 32)
+	if err != nil {
+		return 0, fmt.Errorf("environment variable %v could not be parsed to int value", key)
+	}
+
+	return int(intValue), nil
+}
+
+func GetIntOrDefault(key string, defaultValue int) int {
+	value, err := GetIntOrError(key)
+	if err != nil {
+		return defaultValue
+	}
+
+	return value
+}
+
+func GetIntOrPanic(key string) int {
+	value, err := GetIntOrError(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return value
+}
+
+func GetFloat64OrError(key string) (float64, error) {
+	value := os.Getenv(key)
+	if value == "" {
+		return 0, fmt.Errorf("environment variable %v is empty", key)
+	}
+
+	floatValue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return 0, fmt.Errorf("environment variable %v could not be parsed to int value", key)
+	}
+
+	return floatValue, nil
+}
+
+func GetFloat64OrDefault(key string, defaultValue float64) float64 {
+	value, err := GetFloat64OrError(key)
+	if err != nil {
+		return defaultValue
+	}
+
+	return value
+}
+
+func GetFloat64OrPanic(key string) float64 {
+	value, err := GetFloat64OrError(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return value
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/robin-mbg/env
+
+go 1.19
